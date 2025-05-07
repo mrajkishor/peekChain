f51c0e4935e7faeb781317b0f599a1dadcfa8aaa
Rajkishore <mrajkishor331@gmail.com>
2025-05-07 12:50:41 +0530

    asdf

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const account = {
     }
 }
 
+account.state = "avtive";
 
-account?.profile?.someFun?.()?.name;
 
-account?.profile.someFun();
+account?.profile?.someFun?.().name;
+
+account?.profile?.someFun();
 
 // Fixes
 // 1 . account?.profile?.someFun(); // last funciton shoud n't be optional check
