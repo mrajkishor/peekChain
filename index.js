db53166ce9882c44819288b938a17a3e023d155b
Rajkishore <mrajkishor331@gmail.com>
2025-05-09 18:25:22 +0530

    test1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import localUtil from './utils';
 
 
-const result1 = localUtil.fetchData.name.value.type.core.star;
+const result1 = localUtil.fetchData.name.value.type.core.star.tt;
 
 console.log(result1);
 
