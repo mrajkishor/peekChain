7b1c165bd5a9d1e4e019922b7de72f26a01f9c87
Rajkishore <mrajkishor331@gmail.com>
2025-05-09 16:00:34 +0530

    adsf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // import special from 'dom.service.local';
 import localUtil from './utils';
 
-const result1 = localUtil.fetchData.name; // ❌ unsafe dsa asd
+const result1 = localUtil.fetchData.name; // ❌ unsafe dsa asd d
 // const result2 = externalLib?.get.name;     // ✅ safe
 // const result3 = special?.thing.name;       // ❌ should be skipped adsf
 
