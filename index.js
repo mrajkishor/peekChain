b04b7122e9a3e80f6859fda6fa63ce81673b7833
Rajkishore <mrajkishor331@gmail.com>
2025-05-09 17:29:52 +0530

    test

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-import externalLib from 'axios';
-import special from 'dom.service.local';
+
 import localUtil from './utils';
 
 
-const result1 = localUtil.fetchData.name; // test
-const result2 = externalLib?.get.name;
-const result3 = special?.thing.name;
-console.log(result1, result2, result3); // test
+const result1 = localUtil.fetchData.name;
 
-
-
-const user = null;
-const list = null;
-const users = null;
-const config = null;
-console.log(user.name);
-console.log(user.profile.name);
-console.log(list[0]);
-console.log(users[0].name);
-user.getProfile?.();
-config.api.fetch?.();
-
+console.log(result1);
 
