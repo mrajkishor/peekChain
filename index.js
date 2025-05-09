c16c5a65484e5f4aefce1723d204008cc2d0314c
Rajkishore <mrajkishor331@gmail.com>
2025-05-09 17:20:01 +0530

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import special from 'dom.service.local';
 import localUtil from './utils';
 
 
-const result1 = localUtil.fetchData.name;
+const result1 = localUtil.fetchData.name; // test
 const result2 = externalLib?.get.name;
 const result3 = special?.thing.name;
 console.log(result1, result2, result3);
@@ -14,7 +14,6 @@ const user = null;
 const list = null;
 const users = null;
 const config = null;
-console.log(user?.name?.profile()); // test
 console.log(user.name);
 console.log(user.profile.name);
 console.log(list[0]);
